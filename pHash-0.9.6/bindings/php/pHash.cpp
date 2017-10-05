6e1c628eb8564d255aa910600c8db963aa7cc99e
charlizesmith <charlizesmithweb@gmail.com>
2017-10-05 12:03:53 +0530

    Update pHash.cpp

--- pHash-0.9.6/bindings/php/pHash.cpp
+++ pHash-0.9.6/bindings/php/pHash.cpp
@@ -330,7 +330,7 @@ PHP_FUNCTION(ph_mh_imagehash)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &file, &file_len) == FAILURE) {
 		return;
 	}
-	uint8_t *hash = ph_mh_imagehash(file, *hash,1.0,2.0);
+	uint8_t *hash = ph_mh_imagehash(file, *hash,2.0,1.0);
 	int ret = (uint8_t *)malloc(sizeof(uint8_t));
 	if(ret != 0) {
 		free(hash);
